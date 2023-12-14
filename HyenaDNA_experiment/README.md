043977bab43e585ea29ca9e2bf426ded3bb660c4
Katarina Gresova <514001@mail.muni.cz>
2023-12-14 15:59:29 +0000

    conda setup

---
+++ HyenaDNA_experiment/README.md
@@ -0,0 +1,13 @@
+## Prerequisities
+
+- unix operating system
+- python (idealy 3.8)
+- conda
+
+## Environment 
+
+We use `conda` environment to keep our packages in correct versions. There are a few steps needed to setup a proper `conda` environment and they are in the `prepare_conda.sh` script. In the first line of script, you can change the name of environment.
+
+Run:
+
+`. prepare_conda.sh`
---
+++ HyenaDNA_experiment/prepare_conda.sh
@@ -0,0 +1,11 @@
+ENV_NAME="hyena-dna"
+
+conda create -n $ENV_NAME python=3.8 ipykernel nb_conda_kernels
+source activate $ENV_NAME
+conda install pytorch==1.13.0 torchvision==0.14.0 torchaudio==0.13.0 pytorch-cuda=11.7 -c pytorch -c nvidia
+
+wget https://raw.githubusercontent.com/HazyResearch/hyena-dna/main/requirements.txt
+pip install -r requirements.txt
+rm requirements.txt
+
+# TODO: install Flash Attention - https://github.com/HazyResearch/hyena-dna?tab=readme-ov-file#dependencies
