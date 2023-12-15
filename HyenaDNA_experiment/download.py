4473633d0d377a6daf8073743be5744448564b9f
Katarina Gresova <514001@mail.muni.cz>
2023-12-15 13:29:21 +0000

    storing to proper folders

--- HyenaDNA_experiment/download.py
+++ HyenaDNA_experiment/download.py
@@ -2,29 +2,33 @@ import gdown
 import urllib.request
 import os 
 
+data_dir = "./data"
+if not os.path.exists(data_dir + "/" + model):
+    print("Creating folder for storing data.")
+    os.makedirs(data_dir + "/" + model)
+
 print("Downloading 3utr.sequences.refseq_id.mirna_fc.pkl")
 url = 'https://drive.google.com/file/d/13DS_s2ZXxBGz0DbWsCxRJWaoabUpFsr9/view?usp=drive_link'
-output = '3utr.sequences.refseq_id.mirna_fc.pkl'
+output = 'data/3utr.sequences.refseq_id.mirna_fc.pkl'
 gdown.download(url, output, quiet=True, fuzzy=True)
-
 
 print("Downloading standalone_hyenadna.py")
 url = "https://raw.githubusercontent.com/HazyResearch/hyena-dna/main/standalone_hyenadna.py"
-output = "standalone_hyenadna.py"
+output = "src/standalone_hyenadna.py"
 urllib.request.urlretrieve(url, output)
 
-directory = "./checkpoints"
+downloaded_model_dir = "./checkpoints"
 model = "hyenadna-small-32k-seqlen"
-print("Creating folder for storing downloaded model.")
-if not os.path.exists(directory + "/" + model):
-    os.makedirs(directory + "/" + model)
+if not os.path.exists(downloaded_model_dir + "/" + model):
+    print("Creating folder for storing downloaded model.")
+    os.makedirs(downloaded_model_dir + "/" + model)
     
 print("Downloading HyenaDNA model")
 url = "https://huggingface.co/LongSafari/hyenadna-small-32k-seqlen/resolve/main/weights.ckpt"
-output = directory + "/" + model + "/weights.ckpt"
+output = downloaded_model_dir + "/" + model + "/weights.ckpt"
 urllib.request.urlretrieve(url, output)
 
 print("Downloading config for a HyenaDNA model")
 url = "https://huggingface.co/LongSafari/hyenadna-small-32k-seqlen/raw/main/config.json"
-output = directory + "/" + model + "/config.json"
+output = downloaded_model_dir + "/" + model + "/config.json"
 urllib.request.urlretrieve(url, output)
