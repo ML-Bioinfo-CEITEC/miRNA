e45cd9cc0b9aefdde0f8cdb1d232a8fae0cac0b0
Katarina Gresova <514001@mail.muni.cz>
2023-12-15 13:30:40 +0000

    helper functions for using HyenaDNA model

---
+++ HyenaDNA_experiment/src/hyenadna_helpers.py
@@ -0,0 +1,223 @@
+import os
+import re
+import torch
+import torch.optim as optim
+import subprocess
+import json
+from standalone_hyenadna import CharacterTokenizer, HyenaDNAModel
+from transformers import PreTrainedModel
+from sklearn.metrics import precision_score, recall_score, f1_score
+
+class HyenaDNAPreTrainedModel(PreTrainedModel):
+    """
+    An abstract class to handle weights initialization and a simple interface for downloading and loading pretrained
+    models.
+    """
+    base_model_prefix = "hyenadna"
+
+    def __init__(self, config):
+        pass
+
+    def forward(self, input_ids, **kwargs):
+        return self.model(input_ids, **kwargs)
+
+    @classmethod
+    def from_pretrained(cls,
+                        path,
+                        model_name,
+                        download=False,
+                        config=None,
+                        device='cpu',
+                        use_head=False,
+                        n_classes=2,
+                      ):
+        # first check if it is a local path
+        pretrained_model_name_or_path = os.path.join(path, model_name)
+        if os.path.isdir(pretrained_model_name_or_path) and download == False:
+            if config is None:
+                config = json.load(open(os.path.join(pretrained_model_name_or_path, 'config.json')))
+        else:
+            hf_url = f'https://huggingface.co/LongSafari/{model_name}'
+
+            subprocess.run(f'rm -rf {pretrained_model_name_or_path}', shell=True)
+            command = f'mkdir -p {path} && cd {path} && git lfs install && git clone {hf_url}'
+            subprocess.run(command, shell=True)
+
+            if config is None:
+                config = json.load(open(os.path.join(pretrained_model_name_or_path, 'config.json')))
+
+        scratch_model = HyenaDNAModel(**config, use_head=use_head, n_classes=n_classes)  # the new model format
+        loaded_ckpt = torch.load(
+            os.path.join(pretrained_model_name_or_path, 'weights.ckpt'),
+            map_location=torch.device(device)
+        )
+
+        # need to load weights slightly different if using gradient checkpointing
+        if config.get("checkpoint_mixer", False):
+            checkpointing = config["checkpoint_mixer"] == True or config["checkpoint_mixer"] == True
+        else:
+            checkpointing = False
+
+        # grab state dict from both and load weights
+        state_dict = load_weights(scratch_model.state_dict(), loaded_ckpt['state_dict'], checkpointing=checkpointing)
+
+        # scratch model has now been updated
+        scratch_model.load_state_dict(state_dict)
+        print("Loaded pretrained weights ok!")
+        return scratch_model
+
+
+# helper 1
+def inject_substring(orig_str):
+    """Hack to handle matching keys between models trained with and without
+    gradient checkpointing."""
+
+    # modify for mixer keys
+    pattern = r"\.mixer"
+    injection = ".mixer.layer"
+
+    modified_string = re.sub(pattern, injection, orig_str)
+
+    # modify for mlp keys
+    pattern = r"\.mlp"
+    injection = ".mlp.layer"
+
+    modified_string = re.sub(pattern, injection, modified_string)
+
+    return modified_string
+
+# helper 2
+def load_weights(scratch_dict, pretrained_dict, checkpointing=False):
+    """Loads pretrained (backbone only) weights into the scratch state dict."""
+
+    # loop thru state dict of scratch
+    # find the corresponding weights in the loaded model, and set it
+
+    # need to do some state dict "surgery"
+    for key, value in scratch_dict.items():
+        if 'backbone' in key:
+            # the state dicts differ by one prefix, '.model', so we add that
+            key_loaded = 'model.' + key
+            # breakpoint()
+            # need to add an extra ".layer" in key
+            if checkpointing:
+                key_loaded = inject_substring(key_loaded)
+            try:
+                scratch_dict[key] = pretrained_dict[key_loaded]
+            except:
+                raise Exception('key mismatch in the state dicts!')
+
+    # scratch_dict has been updated
+    return scratch_dict
+
+def load_model(pretrained_model_name = 'hyenadna-small-32k-seqlen',
+               use_padding = True,
+               rc_aug = False,
+               add_eos = False,
+               use_head = False,
+               n_classes = 2,
+               backbone_cfg = None,
+               download=False):
+    
+    max_lengths = {
+        'hyenadna-tiny-1k-seqlen': 1024,
+        'hyenadna-small-32k-seqlen': 32768,
+        'hyenadna-medium-160k-seqlen': 160000,
+        'hyenadna-medium-450k-seqlen': 450000,  # T4 up to here
+        'hyenadna-large-1m-seqlen': 1_000_000,  # only A100 (paid tier)
+    }
+
+    max_length = max_lengths[pretrained_model_name]  # auto selects
+
+    # data settings:
+    # rc_aug = False  # reverse complement augmentation
+    # add_eos = False  # add end of sentence token
+
+    # we need these for the decoder head, if using
+    # use_head = False
+    # n_classes = 2  # not used for embeddings only
+
+    # you can override with your own backbone config here if you want,
+    # otherwise we'll load the HF one in None
+    # backbone_cfg = None
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print("Using device:", device)
+
+    # instantiate the model (pretrained here)
+    if pretrained_model_name in ['hyenadna-tiny-1k-seqlen',
+                                 'hyenadna-small-32k-seqlen',
+                                 'hyenadna-medium-160k-seqlen',
+                                 'hyenadna-medium-450k-seqlen',
+                                 'hyenadna-large-1m-seqlen']:
+        # use the pretrained Huggingface wrapper instead
+        model = HyenaDNAPreTrainedModel.from_pretrained(
+            '../checkpoints',
+            pretrained_model_name,
+            download=download,
+            config=backbone_cfg,
+            device=device,
+            use_head=use_head,
+            n_classes=n_classes,
+        )
+
+    # from scratch
+    elif pretrained_model_name is None:
+        model = HyenaDNAModel(**backbone_cfg, use_head=use_head, n_classes=n_classes)
+        
+    return model, max_length
+        
+
+def get_tokenizer(max_length):
+    return CharacterTokenizer(
+        characters=['A', 'C', 'G', 'T', 'N'],  # add DNA characters, N is uncertain
+        model_max_length=max_length + 2,  # to account for special tokens, like EOS
+        add_special_tokens=False,  # we handle special tokens elsewhere
+        padding_side='left', # since HyenaDNA is causal, we pad on the left
+    )
+
+def train(model, device, train_loader, optimizer, epoch, loss_fn, log_interval=10):
+    """Training loop."""
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = loss_fn(output, target.squeeze())
+        loss.backward()
+        optimizer.step()
+        if batch_idx % log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+            
+def test_metrics(model, device, test_loader, loss_fn):
+    """Test loop."""
+    model.eval()
+    test_loss = 0
+    correct = 0
+    all_predictions = []
+    all_targets = []
+    
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += loss_fn(output, target.squeeze()).item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+            
+            # Collect predictions and targets for later computation of precision, recall, and F1
+            all_predictions.extend(pred.cpu().numpy().flatten())
+            all_targets.extend(target.cpu().numpy())
+
+    test_loss /= len(test_loader.dataset)
+
+    precision = precision_score(all_targets, all_predictions, average='weighted')
+    recall = recall_score(all_targets, all_predictions, average='weighted')
+    f1 = f1_score(all_targets, all_predictions, average='weighted')
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    print('Precision: {:.4f}, Recall: {:.4f}, F1 Score: {:.4f}\n'.format(precision, recall, f1))
