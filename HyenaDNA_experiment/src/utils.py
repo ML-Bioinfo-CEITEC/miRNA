fe006d5ba79d99c3abfb26a265744681812b7e00
Katarina Gresova <514001@mail.muni.cz>
2023-12-24 05:33:09 +0000

    move column constants to utils

--- HyenaDNA_experiment/src/utils.py
+++ HyenaDNA_experiment/src/utils.py
@@ -9,6 +9,13 @@ MIRNAS_NAME_SEQUENCE_DICT = {'hsa-miR-16-5p': 'UAGCAGCACGUAAAUAUUGGCG',
                              'hsa-let-7c-5p': 'UGAGGUAGUAGGUUGUAUGGUU', 
                              'hsa-miR-103a-3p': 'AGCAGCAUUGUACAGGGCUAUGA'}
 
+REGRESSION_LABEL_COLUMN = 'log2fold'
+MRNA_SEQ_COLUMN = 'utr3'
+MIRNA_SEQ_COLUMN = 'miRNA'
+COLUMNS_TO_KEEP_FROM_INPUT_DATA = [MIRNA_SEQ_COLUMN, MRNA_SEQ_COLUMN, REGRESSION_LABEL_COLUMN]
+REGRESSION_LABEL_COLUMN_CLAMPED_TO_ZERO = 'log2fold_clamped'
+CLASSIFICATION_LABEL_COLUMN = 'cls_label'
+
 def load_data_from_pickle(filepath):
     with open(filepath, 'rb') as f:
         data = pickle.load(f)
