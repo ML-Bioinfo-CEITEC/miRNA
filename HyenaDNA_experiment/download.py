6fe24ed35d14e148fbdb623bfe2469d938d14c39
Katarina Gresova <514001@mail.muni.cz>
2023-12-29 11:45:35 +0000

    fixing data path

--- HyenaDNA_experiment/download.py
+++ HyenaDNA_experiment/download.py
@@ -3,9 +3,9 @@ import urllib.request
 import os 
 
 data_dir = "./data"
-if not os.path.exists(data_dir + "/" + model):
+if not os.path.exists(data_dir):
     print("Creating folder for storing data.")
-    os.makedirs(data_dir + "/" + model)
+    os.makedirs(data_dir)
 
 print("Downloading 3utr.sequences.refseq_id.mirna_fc.pkl")
 url = 'https://drive.google.com/file/d/13DS_s2ZXxBGz0DbWsCxRJWaoabUpFsr9/view?usp=drive_link'
