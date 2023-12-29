0aec9113a6d0351c555f07889648caa8bc943b86
Katarina Gresova <514001@mail.muni.cz>
2023-12-29 11:53:34 +0000

    remove installing hyenadna

--- HyenaDNA_experiment/prepare_conda.sh
+++ HyenaDNA_experiment/prepare_conda.sh
@@ -4,12 +4,11 @@ conda create -n $ENV_NAME python=3.8 ipykernel ipywidgets nb_conda_kernels
 source activate $ENV_NAME
 conda install pytorch==1.13.0 torchvision==0.14.0 torchaudio==0.13.0 pytorch-cuda=11.7 -c pytorch -c nvidia
 
-wget https://raw.githubusercontent.com/HazyResearch/hyena-dna/main/requirements.txt
-pip install -r requirements.txt
-rm requirements.txt
-
-# TODO: install Flash Attention - https://github.com/HazyResearch/hyena-dna?tab=readme-ov-file#dependencies
-
+pip install matplotlib
+pip install scikit-learn
+pip install biopython
+pip install pandas
+pip install gdown # for downloading initial data
 pip install comet_ml # for logging training
-#pip install optuna # for hyperparameter tuning
 pip install lightning # Pytorch Lightning for handling models
+pip install transformers # for using HyenaDNA model from HF
