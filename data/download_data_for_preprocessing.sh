517c96c587d26eba3ac447954187b1b0dd95b80e
David Cechak <davidcechak@gmail.com>
2024-07-10 12:55:17 +0200

    Add preprocessing data download script

---
+++ data/download_data_for_preprocessing.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Function to check if gdown is installed
+is_gdown_installed() {
+    python -c "import gdown" &> /dev/null
+    return $?
+}
+
+# Check if gdown is installed
+if is_gdown_installed; then
+    echo "Package 'gdown' is already installed."
+else
+    echo "Package 'gdown' is not installed. Installing..."
+    pip install gdown
+fi
+
+
+# UCSC id map
+# https://drive.google.com/file/d/1geMB7JMUkkDwtwulCw01ruSLqMBEdXiT/view?usp=sharing
+gdown "1geMB7JMUkkDwtwulCw01ruSLqMBEdXiT&confirm=t" --output "GRCh37.p13 hg19/UCSC/id_map"
