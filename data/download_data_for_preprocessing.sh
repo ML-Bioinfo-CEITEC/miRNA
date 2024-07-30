194aed0cd97ff6c517be2da58f27b71add4011f8
davidcechak <davidcechak@gmail.com>
2024-07-30 16:51:27 +0200

    Add all datasources to the download_data_for_preprocessing.sh

--- data/download_data_for_preprocessing.sh
+++ data/download_data_for_preprocessing.sh
@@ -16,6 +16,7 @@ fi
 
 # Create folder structure
 mkdir -p "data/GRCh37.p13 hg19/UCSC"
+mkdir -p "data/target_scan_8"
 
 # UCSC id map
 # https://drive.google.com/file/d/1geMB7JMUkkDwtwulCw01ruSLqMBEdXiT/view?usp=sharing
@@ -24,3 +25,8 @@ gdown "1geMB7JMUkkDwtwulCw01ruSLqMBEdXiT&confirm=t" --output "data/GRCh37.p13 hg
 # utr_sequences_hg19
 # https://drive.google.com/file/d/11PFjBLQJ-rUF8nioh7pd1MzDs1fsSCPG/view?usp=sharing
 gdown "11PFjBLQJ-rUF8nioh7pd1MzDs1fsSCPG&confirm=t" --output "data/GRCh37.p13 hg19/utr_sequences_hg19.txt"
+
+wget -P /data/target_scan_8/Conserved_Site_Context_Scores.txt.zip https://www.targetscan.org/vert_80/vert_80_data_download/Conserved_Site_Context_Scores.txt.zip
+wget -P /data/target_scan_8/Nonconserved_Site_Context_Scores.txt.zip https://www.targetscan.org/vert_80/vert_80_data_download/Nonconserved_Site_Context_Scores.txt.zip
+wget -P /data/target_scan_8/Predicted_Targets_Context_Scores.txt.zip https://www.targetscan.org/vert_80/vert_80_data_download/Predicted_Targets_Context_Scores.default_predictions.txt.zip
+
